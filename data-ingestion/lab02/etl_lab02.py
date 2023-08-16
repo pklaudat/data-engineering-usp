6b5c2b8edf9e58a894806b906548ef1832ca5ad3
Paulo Klaudat <pauloklaudat@gmail.com>
2023-08-15 23:09:58 -0300

    feat(usp): changed etl steps

--- data-ingestion/lab02/etl_lab02.py
+++ data-ingestion/lab02/etl_lab02.py
@@ -16,12 +16,11 @@ def transform(bank_df: polars.DataFrame, employee_df: polars.DataFrame, claims_d
     for string_replacement_action in [
         polars.col("Nome").str.replace_all("PRUDENCIAL", ""), # remove the word PRUDENCIAL from the name
         polars.col("Nome").str.replace_all("(\.+|\/+|\-+)", ""), # remove the special characters
-        polars.col("Nome").str.rstrip() # remove the trailing spaces in the end of the string
+        polars.col("Nome").str.replace_all("SA", ""), # remove the word SA from the name
+        polars.col("Nome").str.rstrip(), # remove the trailing spaces in the end of the string
     ]:
         bank_df = bank_df.with_columns(string_replacement_action)
     
-    # bank_df = bank_df.unique(subset=["CNPJ"])
-
     # ************************************************************************************************ #
     #                                  Claims Data Transformation                                      #
     # ************************************************************************************************ #
@@ -32,15 +31,27 @@ def transform(bank_df: polars.DataFrame, employee_df: polars.DataFrame, claims_d
         claims_df = claims_df.rename(rename_column_action)
     
     for replace_column_action in [
-        polars.col("Nome").str.replace_all("(conglomerado)", ""),
+        polars.col("Nome").str.replace_all("\(conglomerado\)", ""),
         polars.col("Nome").str.replace_all("(\.+|\/+|\-+)", ""), # remove the special characters
+        polars.col("Nome").str.replace_all("Ô", "O"),
         polars.col("Nome").str.replace_all("SA", ""), # remove the word SA from the name
         polars.col("Nome").str.rstrip() # remove the trailing spaces in the end of the string
     ]:
         claims_df = claims_df.with_columns(replace_column_action)
     
-    # claims_df = claims_df.unique(subset=["CNPJ"])
+    # drop null values in the Índex column
+    claims_df = claims_df.drop_nulls(subset=["Índice", "CNPJ"])
 
+    #  average claims by same year and bank
+    claims_df = claims_df.groupby(["Nome", "Ano", "CNPJ"]).agg([
+        polars.col("Quantidade total de reclamações").mean().alias("Quantidade total de reclamações"),
+        polars.col("Tipo").first().alias("Tipo"), # Classificação do Banco
+        # average of the index of claims removing the null values
+        polars.col("Índice").max().alias("Índice"),
+        polars.col("Quantidade total de clientes – CCS e SCR").mean().alias("Quantidade total de clientes – CCS e SCR")
+    ])
+    
+    claims_df.write_csv("data/output/claims.csv")
     # ************************************************************************************************ #
     #                               Employees Data Transformation                                      #
     # ************************************************************************************************ #
@@ -55,31 +66,35 @@ def transform(bank_df: polars.DataFrame, employee_df: polars.DataFrame, claims_d
     # ************************************************************************************************ #
 
     # make join of employee_df and claims_df using the CNPJ and Nome columns
-    bank_claims_df = claims_df.join(bank_df, on=["Nome", "CNPJ"], how="inner")
+    bank_claims_df = claims_df.join(bank_df, on=["Nome","CNPJ"], how="left")
+                
     # make join of bank_claims_df and employee_df using the CNPJ and Nome columns
-    bank_claims_employee_df = bank_claims_df.join(employee_df, on=["Nome"], how="left")
+    bank_claims_employee_df = bank_claims_df.join(employee_df, on=["Nome", "CNPJ"], how="cross")
 
     bank_claims_employee_df = bank_claims_employee_df.select([
         "Nome", 
         "CNPJ", 
-        "Segmento", 
+        "Tipo", # Classificação do Banco
         "Índice", # Indice de reclamações 
+        # "Quantidade total de clientes – CCS e SCR", # Quantidade de Clientes do Bancos
         "Quantidade total de reclamações", # Quantidade de reclamações
         "Recomendam para outras pessoas(%)", # Indice de satisfação dos funcionários dos bancos
-        # "Satisfação com salários e benefícios(%)", # Índice de satisfação com salários dos funcionários dos bancos.
+        "Remuneração e benefícios", # Índice de satisfação com salários dos funcionários dos bancos.
     ])
 
     for rename_column_action in [
         {"Índice": "Índice de reclamações"},
         {"Quantidade total de reclamações": "Quantidade de reclamações"},
+        {"Tipo": "Classificação do Banco"},
+        # {"Quantidade total de clientes - CCS e SCR": "Quantidade de Clientes do Banco"},
         {"Recomendam para outras pessoas(%)": "Índice de satisfação dos funcionários dos bancos"},
+        {"Remuneração e benefícios": "Índice de satisfação com salários dos funcionários dos bancos"}
     ]:
         bank_claims_employee_df = bank_claims_employee_df.rename(rename_column_action)
 
     # remove duplicated lines by the columns Nome and CNPJ
-    bank_claims_employee_df = bank_claims_employee_df.unique(subset=["CNPJ"])
+    bank_claims_employee_df = bank_claims_employee_df.unique(subset=["Nome", "CNPJ"])
 
-    breakpoint()
     # Final Table must contain the following columns:
     # Nome do Banco
     # CNPJ
